8898e0e1656fb95c4f95189d4cd6df17101d1dac
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-12-04 12:05:16 +0800

    refactor(home): converted into arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <h1>Home Page</h1>
     </div>
   );
-}
+};
 
 export default App;
