f02626d3012af9caad96b2254b3a6cbb9172ad2c
ANTHONY JOHN APARICIO <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-12-28 17:36:09 +0800

    feat(home): added logged in status

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const App: React.FC = () => {
+  let loggedIn = false;
+  const getStatus = async () => {
+    const res = await fetch('/me');
+    const data = await res.json();
+    loggedIn = data.status;
+  };
+
+  useEffect(() => {
+    getStatus();
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <h1>Home Page</h1>
+      {loggedIn ? <h1>You are logged in</h1> : <h1>You are not logged in.</h1>}
     </div>
   );
 };
