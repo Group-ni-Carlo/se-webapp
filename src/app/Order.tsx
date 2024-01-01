e44da6db9eb35ac3faf40f909a3c2608cf1cdb53
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-01-02 01:20:12 +0800

    feat: added fetch for upload

--- src/app/Order.tsx
+++ src/app/Order.tsx
@@ -3,6 +3,26 @@ import React, { Fragment } from 'react';
 import OrderForm from '../components/merch/OrderForm';
 
 const Order: React.FC = () => {
+  const submit = async (e: React.FormEvent, file: File | null) => {
+    e.preventDefault();
+
+    const formData: FormData = new FormData();
+    formData.append('image', file!);
+
+    const uploadOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await fetch(
+      'http://localhost:5000/order/upload',
+      uploadOptions
+    );
+    const data = res.json();
+  };
+
   return (
     <Fragment>
       <div className="flex flex-row flex-1 justify-center items-center">
