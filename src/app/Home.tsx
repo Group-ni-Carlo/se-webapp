59bc642f597d57be86deda3ad5c95af7b5d6bb2e
Jezerwel <jezerwel.grino-22@cpu.edu.ph>
2024-01-02 22:56:45 +0800

    refactor(imports): updated imports to renamed files

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,59 +1,24 @@
 import { Fragment } from 'react';
-import MiddleBlock from '../components/MiddleBlock';
 import Container from '../common/Container';
-import ScrollToTop from '../common/ScrollToTop';
-import ContentBlock from '../components/ContentBlock';
-import IntroContent from '../content/IntroContent.json';
-import AboutUs from '../content/AboutUs.json';
-import RecentEvents from '../content/RecentEvents.json';
-import PartnersAndSponsors from '../content/PartnersAndSponsors.json';
-import Organizations from '../content/Organizations.json';
-import ContactContent from '../content/ContactContent.json';
+import Events from '../components/RecentEvents/index';
+import Organizations from '../components/Organizations/index';
+import Partners from '../components/Partners/index';
+import IntroContent from '../components/Intro/index';
+import AboutUs from '../components/AboutUs/index';
 import Footer from '../components/Footer/';
 import Header from '../components/Header/';
 import '../index.css';
+
 const Home = () => {
   return (
     <Fragment>
       <Header />
       <Container>
-        <ScrollToTop />
-        <ContentBlock
-          type="right"
-          title={IntroContent.title}
-          content={IntroContent.text}
-          button={IntroContent.button}
-          icon="image.svg"
-          id="intro"
-        />
-        <MiddleBlock
-          title={AboutUs.title}
-          content={AboutUs.text}
-          button={AboutUs.button}
-        />
-        <ContentBlock
-          type="left"
-          title={RecentEvents.title}
-          content={RecentEvents.text}
-          section={RecentEvents.section}
-          icon="image.svg"
-          id="about"
-        />
-        <ContentBlock
-          type="right"
-          title={PartnersAndSponsors.title}
-          content={PartnersAndSponsors.text}
-          icon="image.svg"
-          id="mission"
-        />
-        <ContentBlock
-          type="left"
-          title={Organizations.title}
-          content={Organizations.text}
-          section={Organizations.section}
-          icon="image.svg"
-          id="product"
-        />
+        <IntroContent />
+        <AboutUs />
+        <Events />
+        <Partners />
+        <Organizations />
       </Container>
       <Footer />
     </Fragment>
