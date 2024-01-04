d1112557d291ebe2849971b9ae9188c672ca3bb5
Carlow18 <124373867+Carlow18@users.noreply.github.com>
2024-01-05 06:29:00 +0800

    feat(AdminMerch): added pop up window when clicking a merch

--- src/components/merchandise/MerchPage.tsx
+++ src/components/merchandise/MerchPage.tsx
@@ -13,6 +13,7 @@ const Product: React.FC = () => {
     }[]
   >([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [formData, setFormData] = useState({
     price: 0,
@@ -20,8 +21,9 @@ const Product: React.FC = () => {
     description: '',
     stock: 0
   });
-
+  const [activeProductId, setActiveProductId] = useState<number | null>(null);
   const [imageFile, setImageFile] = useState<File | null>(null);
+  const [selectedProduct, setSelectedProduct] = useState<any>(null);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -220,28 +222,131 @@ const Product: React.FC = () => {
           </button>
         </div>
         <div className="m-20">
-          <div className="flex flex-wrap space-x-32">
+          <div className="grid grid-cols-4 gap-4">
             {products.map((product, index) => (
               <div key={index} className="w-64">
-                <div className="w-full h-48">
+                <div
+                  className="flex flex-col items-center justify-center"
+                  onClick={() => {
+                    setSelectedProduct(product);
+                    setShowModal(true);
+                  }}
+                >
+                  <div className="w-64 h-48">
+                    <img
+                      className="object-cover w-full h-full"
+                      src={`http://localhost:5000/uploads/${product.image}`}
+                      alt=""
+                    />
+                  </div>
+                  <h2
+                    className="font cursor-pointer"
+                    onClick={() => {
+                      setSelectedProduct(product);
+                      setShowModal(true);
+                    }}
+                  >
+                    {product.title}
+                  </h2>
+                </div>
+
+                {activeProductId === product.id && (
+                  <div className="mt-4">
+                    <p>{product.description}</p>
+                    <p>Price: ${product.price}</p>
+                    <p>In Stock: {product.stock}</p>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+        {selectedProduct && showModal && (
+          <Modal
+            onClose={() => {
+              setSelectedProduct(null);
+              setShowModal(false);
+            }}
+            product={selectedProduct}
+          />
+        )}
+      </div>
+    </Fragment>
+  );
+};
+
+interface ProductProps {
+  id: number;
+  image: string;
+  price: number;
+  title: string;
+  description: string;
+  stock: number;
+}
+
+interface ModalProps {
+  product: ProductProps;
+  onClose: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ product, onClose }) => {
+  return (
+    <div
+      className="fixed z-10 inset-0 overflow-y-auto"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          aria-hidden="true"
+        ></div>
+        <span
+          className="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+        >
+          &#8203;
+        </span>
+        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3
+                  className="text-lg leading-6 font-medium text-gray-900"
+                  id="modal-title"
+                >
+                  {product.title}
+                </h3>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">{product.description}</p>
+                  <p className="text-sm text-gray-500">
+                    Price: ${product.price}
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    In Stock: {product.stock}
+                  </p>
                   <img
                     className="w-full h-full object-cover"
                     src={`http://localhost:5000/uploads/${product.image}`}
                     alt=""
                   />
                 </div>
-                <div className="p-4">
-                  <h2>{product.title}</h2>
-                  <p>{product.description}</p>
-                  <p>Price: ${product.price}</p>
-                  <p>In Stock: {product.stock}</p>
-                </div>
               </div>
-            ))}
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <button
+              type="button"
+              className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              onClick={onClose}
+            >
+              Close
+            </button>
           </div>
         </div>
       </div>
-    </Fragment>
+    </div>
   );
 };
 
