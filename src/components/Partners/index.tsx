3d44ba9735803173b37c971b1dd566596f59bd4c
Jezerwel <jezerwel.grino-22@cpu.edu.ph>
2024-01-05 16:27:09 +0800

    refactor: add carousel for images

--- src/components/Partners/index.tsx
+++ src/components/Partners/index.tsx
@@ -1,6 +1,8 @@
 import { Row, Col } from 'antd';
 import { Fade } from 'react-awesome-reveal';
 import logo from '../../assets/img/icons/2922280_27002.jpg';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const FadeAny = Fade as any;
 
@@ -17,17 +19,26 @@ const Partners = () => {
               <p className="my-12">
                 Relevant information about the organization and its activities
               </p>
-              <div className="flex justify-between items-center max-w-full">
-                <Row justify="center" align="middle">
-                  <Col span={11}>
-                    <div className="flex justify-center gap-5">
-                      <img src={logo} width="150px" height="150px" />
-                      <img src={logo} width="150px" height="150px" />
-                      <img src={logo} width="150px" height="150px" />
-                    </div>
-                  </Col>
-                </Row>
-              </div>
+              <Carousel className="mx-auto w-3/4" showArrows infiniteLoop>
+                <div>
+                  <img src={logo} />
+                </div>
+                <div>
+                  <img src={logo} />
+                </div>
+                <div>
+                  <img src={logo} />
+                </div>
+                <div>
+                  <img src={logo} />
+                </div>
+                <div>
+                  <img src={logo} />
+                </div>
+                <div>
+                  <img src={logo} />
+                </div>
+              </Carousel>
             </div>
           </Col>
         </Row>
