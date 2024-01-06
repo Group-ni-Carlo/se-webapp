b882166021a3527572841629f40e3d87c6473d46
ANTHONY JOHN APARICIO <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-01-06 16:49:38 +0800

    refactor: updated impports

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import checkIfLoggedIn from '../components/auth/checkIfLoggedIn';
-import Login from '../components/login/Login';
+import Login from '../components/login/login';
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
--- src/app/Register.tsx
+++ src/app/Register.tsx
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 
 import checkIfLoggedIn from '../components/auth/checkIfLoggedIn';
-import Register from '../components/register/Register';
+import Register from '../components/register/register';
 
 const RegisterPage: React.FC = () => {
   const navigate = useNavigate();
